8aea1a00a9336a357f9235d0a8f12abf8fbf7001
robindewitte <44872286+robindewitte@users.noreply.github.com>
2021-06-07 14:45:56 +0200

    Update Program.cs

--- twatter-postservice/Program.cs
+++ twatter-postservice/Program.cs
@@ -20,7 +20,8 @@ namespace twatter_postservice
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://*:5005");
                 });
     }
 }
